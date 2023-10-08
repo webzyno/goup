d3250fe786cc90fd3d7094db8b811e5a9bd6ceb9
趙子賢 <danny900714@gmail.com>
2023-10-08 18:49:51 +0800

    Finish GoReleaser GitHub checker

--- check.go
+++ check.go
@@ -1,13 +1,17 @@
 package goup
 
+import "errors"
+
+var ErrAmbiguousRelease = errors.New("multiple releases were selected, but checker only allows one release")
+
 type Checker interface {
-	GetLatestRelease() (Release, error)
+	GetLatestRelease() (*Release, error)
 }
 
 type CheckConfig struct {
 	Version string
 }
 
-func Check(checker Checker, config *CheckConfig) (Update, error) {
+func Check(checker Checker, config *CheckConfig) (*Update, error) {
 	panic("not implemented")
 }
