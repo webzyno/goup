690547d20171c2c3e0cef5709622a700922f859a
趙子賢 <danny900714@gmail.com>
2023-10-12 09:18:45 -0500

    Implement restart function (#7)

--- restart.go
+++ restart.go
@@ -1,5 +1,35 @@
 package goup
 
+import (
+	"os"
+	"os/exec"
+	"runtime"
+	"syscall"
+)
+
 func Restart() error {
-	panic("not implemented")
+	self, err := os.Executable()
+	if err != nil {
+		return err
+	}
+
+	// Windows does not support exec syscall.
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command(self, os.Args[1:]...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Env = os.Environ()
+
+		if err := cmd.Start(); err != nil {
+			return err
+		}
+	} else {
+		if err := syscall.Exec(self, os.Args, os.Environ()); err != nil {
+			return err
+		}
+	}
+
+	os.Exit(0)
+	return nil
 }
